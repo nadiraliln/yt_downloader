831322b1116503bf115dd2780181fa4d3217baeb
Nadir ali <nadirali7q12@gmail.com>
2025-06-01 19:08:38 +0500

    add

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://yt-frontend-six.vercel.app/",  # Replace with your Vercel URL
+]
 
 ROOT_URLCONF = 'backend.urls'
 
